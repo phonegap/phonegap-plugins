e730ae3d2474cda806bf1e12b74f1ab170b9b798
Randy McMillan <randy.lee.mcmillan@gmail.com>
2012-02-29 16:56:48 -0500

    Added notes

--- iPhone/ChildBrowser/ChildBrowserCommand.m
+++ iPhone/ChildBrowser/ChildBrowserCommand.m
@@ -1,18 +1,18 @@
-//
-
-// 
-//
 //  Created by Jesse MacFadyen on 10-05-29.
 //  Copyright 2010 Nitobi. All rights reserved.
+//  Copyright (c) 2011, IBM Corporation
+//  Copyright 2011, Randy McMillan
+//  Copyright 2012, Randy McMillan
 //
 
 #import "ChildBrowserCommand.h"
 
 #ifdef PHONEGAP_FRAMEWORK
-	#import <PhoneGap/PhoneGapViewController.h>
-	
+//	#import <PhoneGap/PhoneGapViewController.h> //Reference PhoneGapLib/Classes/PGViewController.m LINE: 46
+#import <PhoneGap/PGViewController.h>
 #else
-	#import "PhoneGapViewController.h"
+//	#import <PhoneGap/PhoneGapViewController.h> TODO add based on PhoneGap version
+#import "PhoneGapViewController.h"
 #endif
 
 
@@ -22,31 +22,43 @@
 
 - (void) showWebPage:(NSMutableArray*)arguments withDict:(NSMutableDictionary*)options // args: url
 {	
-	if(childBrowser == NULL)
+	
+    if(childBrowser == NULL)
 	{
 		childBrowser = [[ ChildBrowserViewController alloc ] initWithScale:FALSE ];
 		childBrowser.delegate = self;
 	}
 	
-/* // TODO: Work in progress
-	NSString* strOrientations = [ options objectForKey:@"supportedOrientations"];
-	NSArray* supportedOrientations = [strOrientations componentsSeparatedByString:@","];
+/*  TODO: Work in progress
+    NSString* strOrientations = [ options objectForKey:@"supportedOrientations"];
+    NSArray* supportedOrientations = [strOrientations componentsSeparatedByString:@","];
 */
 
-	PhoneGapViewController* cont = (PhoneGapViewController*)[ super appViewController ];
-	childBrowser.supportedOrientations = cont.supportedOrientations;
-	[ cont presentModalViewController:childBrowser animated:YES ];
-	
-	NSString *url = (NSString*) [arguments objectAtIndex:0];
-	
-
-	[childBrowser loadURL:url  ];
-
+/* TODO Add back support pre 1.0 Check progress of https://github.com/alunny/ChildBrowser for PhoneGapBuild usage - @RandyMcMillan
+//Pre PhoneGap 1.0 
+//PhoneGapViewController* cont = (PhoneGapViewController*)[ super appViewController ];
+//Reference PhoneGapLib/Classes/PGPlugin.m LINE: 140
+*/
+   
+    PGViewController* cont = (PGViewController*)[ super viewController ]; 
+    childBrowser.supportedOrientations = cont.supportedOrientations;
+    
+    if ([cont respondsToSelector:@selector(presentViewController)]) {
+        //Reference UIViewController.h Line:179 for update to iOS 5 difference - @RandyMcMillan
+        [cont presentViewController:childBrowser animated:YES completion:nil];        
+    } else {
+        [ cont presentModalViewController:childBrowser animated:YES ];
+    }                 
+    
+    NSString *url = (NSString*) [arguments objectAtIndex:0];
+    
+    [childBrowser loadURL:url  ];
+    
 }
 
 -(void) close:(NSMutableArray*)arguments withDict:(NSMutableDictionary*)options // args: url
 {
-	[ childBrowser closeBrowser];
+    [ childBrowser closeBrowser];
 	
 }
 
@@ -68,10 +80,10 @@
 	
 	NSString* tempLoc = [NSString stringWithFormat:@"%@",newLoc];
 	NSString* encUrl = [tempLoc stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
-	 
+    
 	NSString* jsCallback = [NSString stringWithFormat:@"ChildBrowser._onLocationChange('%@');",encUrl];
 	[self.webView stringByEvaluatingJavaScriptFromString:jsCallback];
-
+    
 }
 
 

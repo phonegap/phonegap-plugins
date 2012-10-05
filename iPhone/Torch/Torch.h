43dd3a60eb0467f6b4d836e27503520f5c083033
Stuart Swindells <github@lumo.me.uk>
2012-10-05 10:26:45 +0100

    Remove now-deprecated ifdefs from iPhone Torch plugin

--- iPhone/Torch/Torch.h
+++ iPhone/Torch/Torch.h
@@ -7,22 +7,9 @@
 #import <Foundation/Foundation.h>
 #import <AVFoundation/AVFoundation.h>
 
-#ifdef PHONEGAP_FRAMEWORK
-#import <PhoneGap/PGPlugin.h>
-#endif
-
-#ifdef CORDOVA_FRAMEWORK
 #import <CORDOVA/CDVPlugin.h>
-#endif
 
-#ifdef PHONEGAP_FRAMEWORK
-@interface Torch : PGPlugin {
-#endif
-
-#ifdef CORDOVA_FRAMEWORK
 @interface Torch : CDVPlugin  {
-#endif
-
 	 AVCaptureSession* session;
 }
 
